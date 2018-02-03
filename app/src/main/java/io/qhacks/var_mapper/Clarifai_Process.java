2dd7815347c2d12c58836eb8ba95c0e2c6f07b69
Emilio Kartono <emilio.kartono@mail.utoronto.ca>
2018-02-03 17:13:38 -0500

    wasd

--- app/src/main/java/io/qhacks/var_mapper/Clarifai_Process.java
+++ app/src/main/java/io/qhacks/var_mapper/Clarifai_Process.java
@@ -1,8 +1,37 @@
 package io.qhacks.var_mapper;
 
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import okhttp3.OkHttpClient;
+
 /**
  * Created by james on 2/3/2018.
  */
 
 public class Clarifai_Process {
+    private final String API_KEY = "d584f38ca7ba42d2909b5a149162d790";
+    private ClarifaiClient client;
+
+    public Clarifai_Process(){
+        client = new ClarifaiBuilder(API_KEY)
+                .client(new OkHttpClient()) // OPTIONAL. Allows customization of OkHttp by the user
+                .buildSync();
+    }
+
+    public List<ClarifaiOutput<Concept>> getCategories(byte[] imageBytes){
+        List<ClarifaiOutput<Concept>> predictionResults =
+            client.getDefaultModels().generalModel() // You can also do client.getModelByID("id") to get your custom models
+                    .predict()
+                    .withInputs(
+                            ClarifaiInput.forImage(imageBytes))
+                    .executeSync()
+                    .get();
+
+        return predictionResults;
+    }
 }
