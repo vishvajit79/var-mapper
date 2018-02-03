ed907dc5e99623069aebb4770f322058b68419c6
Vishvajit Kher <vishvajit79@gmail.com>
2018-02-03 07:39:51 -0500

    all activities are updated with new design

--- app/src/main/java/io/qhacks/var_mapper/MainActivity.java
+++ app/src/main/java/io/qhacks/var_mapper/MainActivity.java
@@ -2,19 +2,65 @@ package io.qhacks.var_mapper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+import java.util.List;
+
+import io.qhacks.var_mapper.database.DatabaseHelper;
 
 public class MainActivity extends Activity {
 
+    private SQLiteDatabase sqLiteDatabase;
+    private Cursor cursor;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.main_startBtn);
+        listView = findViewById(R.id.main_showmap_lv);
+        try {
+            SQLiteOpenHelper databaseHelper = new DatabaseHelper(this);
+            sqLiteDatabase = databaseHelper.getReadableDatabase();
+            Toast toast = Toast.makeText(this, "Database available", Toast.LENGTH_SHORT);
+            toast.show();
+            cursor = sqLiteDatabase.rawQuery("select _id, name from map", null);
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"name"},
+                    new int[]{android.R.id.text1},//map the contents of NAME col to text in ListView
+                    0);
+            listView.setAdapter(listAdapter);
+        }catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this,"Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(view.getContext(), ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button button = findViewById(R.id.main_downloadnewmap_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/io/qhacks/var_mapper/ThirdActivity.java
+++ app/src/main/java/io/qhacks/var_mapper/ThirdActivity.java
@@ -1,14 +1,46 @@
 package io.qhacks.var_mapper;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.qhacks.var_mapper.database.DatabaseHelper;
 
 public class ThirdActivity extends Activity {
+    private TextView name;
+    private TextView description;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase sqLiteDatabase;
+    int mallId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        name = findViewById(R.id.third_mallname_txt);
+        description = findViewById(R.id.third_description_txt);
+        Intent intent  = getIntent();
+        mallId = intent.getIntExtra("mallId", 0);
+        databaseHelper = new DatabaseHelper(ThirdActivity.this);
+        sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        //retrieves data from database
+        Cursor cursor = null ;
+        cursor = sqLiteDatabase.rawQuery("SELECT * FROM map WHERE _id = '" + mallId + "'", null);
+
+        if(cursor.getCount() != 0){
+            while (cursor.moveToNext()){
+                name.setText(cursor.getString(1));
+                description.setText("\n\n\n\n " +
+                        "Description : " + cursor.getString(2) + "\n\n\n" +
+                        " Location : " + cursor.getString(3) + "\n\n\n" +
+                        " Store Count : " + cursor.getString(4) + "\n\n\n");
+            }
+        }
     }
 }
