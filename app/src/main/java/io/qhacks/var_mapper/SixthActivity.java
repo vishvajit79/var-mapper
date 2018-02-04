a03cd2d7a422895341e107a8e255aaee966c947c
Vishvajit Kher <vishvajit79@gmail.com>
2018-02-03 21:10:37 -0500

    new activity added

---
+++ app/src/main/java/io/qhacks/var_mapper/SixthActivity.java
@@ -0,0 +1,29 @@
+package io.qhacks.var_mapper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SixthActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sixth);
+
+        Button button = findViewById(R.id.sixth_find_btn);
+        TextView textView = findViewById(R.id.sixth_storename_txt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), FourthActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
